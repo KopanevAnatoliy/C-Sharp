9c609f3fb35bba65a016086527c3c9a7b43973fd
Anatoliy Kopanev <kopanev.rnd@yandex.ru>
2022-08-31 13:35:44 +0300

    added HW1T4-3, HW1T4-4

---
+++ Seminars/Seminar1/HomeWorkTask4-3/Program.cs
@@ -0,0 +1,27 @@
+﻿//=====================================================================
+// №4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из 
+// этих чисел.
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+//=====================================================================
+
+// Считываем данные пользователия.
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+
+// Проверяем переменные на null значения.
+if (inputLineA != null && inputLineB != null && inputLineC != null)
+{
+    // Преобразуем переменные к типу int.
+    int numberA = int.Parse(inputLineA);
+    int numberB = int.Parse(inputLineB);
+    int numberC = int.Parse(inputLineC);
+
+    // Заводим переменную max и с помощью вложенного тернарного оператора ложим в нее максимальное число.
+    int max = numberA > numberB ? numberA > numberC ? numberA : numberC : numberB > numberC ? numberB : numberC;
+
+    // Выводим максимальное число.
+    Console.WriteLine("Максимальное число: " + max);
+}
---
+++ Seminars/Seminar1/HomeWorkTask4-4/Program.cs
@@ -0,0 +1,30 @@
+﻿//=====================================================================
+// №4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из 
+// этих чисел.
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+//=====================================================================
+
+// Считываем данные пользователия.
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+string? inputLineC = Console.ReadLine();
+// Проверяем переменные на null значения.
+if (inputLineA != null && inputLineB != null && inputLineC != null)
+{
+    // Преобразуем переменные к типу int.
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+    int inputNumberC = int.Parse(inputLineC);
+
+    // Заводим переменную max.
+    int max;
+
+    // Определяем максимальное число.
+    max = Math.Max(inputNumberA, inputNumberB);
+    max = Math.Max(max, inputNumberC);
+    
+    // Выводим максимальное число.
+    Console.WriteLine("Максимальное число: " + max);
+}
