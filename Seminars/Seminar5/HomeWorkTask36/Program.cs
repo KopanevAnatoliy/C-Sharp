2b3e2ab50aac043bbeb4e5bf384db6a315b9c409
Anatoliy Kopanev <kopanev.rnd@yandex.ru>
2022-09-14 11:08:19 +0300

    added HW5

---
+++ Seminars/Seminar5/HomeWorkTask36/Program.cs
@@ -0,0 +1,71 @@
+﻿// №36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму 
+// элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19 
+// [3, 7, -2, 1] -> 8
+// [-4, -6, 89, 6] -> 0
+// * Найдите все пары в массиве и выведите пользователю
+
+
+// Генерация случайного массива.
+int[] GenArr(int arrLen, int arrMin, int arrMax)
+{
+    int[] arr = new int[arrLen];
+    Random rnd = new Random();
+
+    if (arrMin > arrMax)
+    {
+        int temp = arrMax;
+        arrMax = arrMin;
+        arrMin = temp;
+    }
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        arr[i] = rnd.Next(arrMin, arrMax);
+    }
+    return arr;
+}
+
+// Печать массива
+void PrintArr(int[] arr, string message = "Массив: ")
+{
+    Console.WriteLine($"{message}[{string.Join(", ", arr)}]");
+}
+
+// Сумма элементов с нечетным индексом.
+int OddIndSum(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+// Поиск повторяющихся элементов.
+void PrintPairItems(int[] arr)
+{
+    Dictionary<int, int> pairItems = new Dictionary<int, int>();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (!(pairItems.TryAdd(arr[i], 1)))
+        {
+            pairItems[arr[i]] += 1;
+        }
+    }
+    
+    Console.Write("Парные элементы:");
+    foreach (var item in pairItems)
+    {   
+        if (item.Value > 1) 
+        Console.Write($" {item.Key}");
+    }
+}
+
+
+int[] arr = GenArr(20, 0, 21);
+PrintArr(arr);
+Console.WriteLine($"Сумма элементов с нечетным индексом: {OddIndSum(arr)}");
+PrintPairItems(arr);
+
---
+++ Seminars/Seminar5/HomeWorkTask38/Program.cs
@@ -0,0 +1,96 @@
+﻿// №38 Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+// минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+// [2 0,4 9 7,2 78] -> 77,6
+// * Отсортируйте массив методом вставки и методом подсчета, а затем найдите 
+// разницу между первым и последним элементом. Для задачи со звездочкой 
+// использовать заполнение массива целыми числами.
+
+
+// Генерация случайного массива.
+int[] GenArr(int arrLen, int arrMin, int arrMax)
+{
+    int[] arr = new int[arrLen];
+    Random rnd = new Random();
+
+    if (arrMin > arrMax)
+    {
+        int temp = arrMax;
+        arrMax = arrMin;
+        arrMin = temp;
+    }
+
+    for (int i = 0; i < arrLen; i++)
+    {
+        arr[i] = rnd.Next(arrMin, arrMax);
+    }
+    return arr;
+}
+
+// Печать массива
+void PrintArr(int[] arr, string message = "Массив: ")
+{
+    Console.WriteLine($"{message}[{string.Join(", ", arr)}]");
+}
+
+// Сортировка вставкой.
+int[] InsertSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        int currentElement = arr[i];
+        int j = i;
+        while (j > 0 && arr[j - 1] > currentElement)
+        {
+            int tmp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = tmp;
+            j--;
+        }
+    }
+    return arr;
+}
+
+// Сортировка подсчетом.
+int[] CountSort(int[] arr)
+{
+    int max = arr.Max();
+    int min = arr.Min();
+
+    int[] count = new int[max - min + 1];
+    int j = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        count[arr[i] - min]++;
+    }
+
+    for (int i = min; i <= max; i++)
+    {
+        while (count[i - min]-- > 0)
+        {
+            arr[j] = i;
+            j++;
+        }
+    }
+
+    return arr;
+}
+
+// Метод для замера времени.
+void TimeTest(Func<int[], int[]> Method, int[] arr, string funcName)
+{
+    DateTime start = DateTime.Now;
+    Method(arr);
+    Console.WriteLine($"Затраченное время метода {funcName}: {(DateTime.Now - start).TotalMilliseconds} ms");
+}  
+
+
+int[] arr = GenArr(10, 0, 5000000);
+PrintArr(arr);
+Console.WriteLine($"Минимальный элемент массива: {arr.Min()}\nМаксимальный элемент массива: {arr.Max()}");
+PrintArr(InsertSort((int[])arr.Clone()), "Массив сортированный методом вставки: ");
+PrintArr(CountSort((int[])arr.Clone()), "Массив сортированный методом подсчета: ");
+TimeTest(InsertSort, (int[])arr.Clone(), "InsertSort");
+TimeTest(CountSort, (int[])arr.Clone(), "CountSort");
+
